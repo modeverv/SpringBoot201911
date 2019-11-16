d6316a912261b7e6cc21367a38c186a5d167ba5c
modeverv <modeverv@gmail.com>
2019-11-16 13:57:34 +0900

    inital endpoint

--- src/main/java/asia/daemon/lovesaemi/tw2/controller/TwitterController.java
+++ src/main/java/asia/daemon/lovesaemi/tw2/controller/TwitterController.java
@@ -1,12 +1,13 @@
 package asia.daemon.lovesaemi.tw2.controller;
 
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TwitterController {
 
-	public String getHello(Model m) {
-
+	@GetMapping("/hello")
+	public String getHello() {
+		return "ok";
 	}
 }
