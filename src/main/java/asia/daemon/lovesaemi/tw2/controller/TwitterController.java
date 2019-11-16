e409b116a847a2a9f3b6e941050ed271ab7919df
modeverv <modeverv@gmail.com>
2019-11-16 13:45:28 +0900

    initial

---
+++ src/main/java/asia/daemon/lovesaemi/tw2/controller/TwitterController.java
@@ -0,0 +1,12 @@
+package asia.daemon.lovesaemi.tw2.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TwitterController {
+
+	public String getHello(Model m) {
+
+	}
+}
---
+++ src/test/java/asia/daemon/lovesaemi/tw2/BackendTwFavApplicationTests.java
@@ -0,0 +1,13 @@
+package asia.daemon.lovesaemi.tw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTwFavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
